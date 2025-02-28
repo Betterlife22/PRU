c1de07d99ab5163740faa5514e304d969b621037
QuocHung <hungnguyen@maple.com.vn>
2025-03-01 04:13:14 +0700

    initial commit

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -1,16 +1,40 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int maxHealth = 100;
+    int currentHealth;
+    [SerializeField] Animator animator;
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        Debug.Log("Enemy hit");
+        animator.SetTrigger("GetHurt");
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
     }
+    void Die()
+    {
+        Debug.Log("Enemy Die");
+        animator.SetBool("IsDie", true);
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+
+        StartCoroutine(DisappearAfterAnimation());
+    }
+
+    System.Collections.IEnumerator DisappearAfterAnimation()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        Destroy(gameObject);
+    }
+
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,13 +7,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private int attackDamage = 20;
     private Animator animator;
     private bool isGrounded;
     private Rigidbody2D rb;
+    private bool isAttacking = false;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    [SerializeField] float attackRate = 2f; 
+    float nextAttackTime = 0f;
+    [SerializeField] float pushForce = 100f;
+    private Knockback knockback;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        knockback = GetComponent<Knockback>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,15 +35,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleMovement();
-        HandleJump();
-        UpdateAnimation();
+        if (!knockback.IsGettingKnockback)
+        {
+            UpdateAnimation();
+            HandleAttack();
+            if (!isAttacking)
+            {
+                HandleMovement();
+                HandleJump();
+            }
+        }
     }
     private void HandleMovement()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
         if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
     }
@@ -52,10 +70,58 @@ public class PlayerController : MonoBehaviour
 
         animator.SetBool("IsRunning", isRunning);
         animator.SetBool("IsJumping", isJumping);
+        animator.SetBool("IsAttacking", isAttacking);
+        
+    }
 
-        if (Input.GetMouseButtonDown(0))
+    void HandleAttack()
+    {
+        if (Time.time >= nextAttackTime)
         {
-            animator.SetTrigger("Attack");
+            if (Input.GetMouseButtonDown(0))
+            {
+                isAttacking = true;
+                if (isGrounded)
+                {
+                    rb.linearVelocity = Vector2.zero;
+                }
+                
+
+            }
         }
+        
+    }
+
+    void EndAttack()
+    {
+        isAttacking = false;
+        animator.SetBool("IsAttacking", isAttacking);
+    }
+
+    void DealDamage()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyAI>().TakeDamage(attackDamage);
+        }
+        nextAttackTime = Time.time + 1f / attackRate;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (enemyLayers == (enemyLayers | (1 << collision.gameObject.layer)))
+        {
+            Debug.Log("Touch Enemy");
+            
+            Vector2 knockbackDirection = (transform.position - collision.transform.position).normalized;
+            Debug.Log(knockbackDirection);
+            HealthSystem.Instance.TakeDamage(10, knockbackDirection);
+
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
