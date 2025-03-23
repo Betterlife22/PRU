c822fc30f5d48ef6e42ed1f47b63d5b5cd13b95d
Ha Tung <haduytung809@gmail.com>
2025-03-23 20:10:44 +0700

    dead menu

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("Enemies"))
         {
+            animator.SetBool(AnimationStrings.isAlive, false);
             Destroy(collision.gameObject);
         }
     }
