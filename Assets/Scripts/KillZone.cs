efc35f1c4825fe51dfb9854141de2fd974f5e1ae
Ha Tung <haduytung809@gmail.com>
2025-03-24 11:17:26 +0700

    fix killzone

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class KillZone : MonoBehaviour
 {
     public Animator animator;
+    public GameManager manager;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("Enemies"))
         {
+            manager.GameOver();
             animator.SetBool(AnimationStrings.isAlive, false);
             Destroy(collision.gameObject);
         }
